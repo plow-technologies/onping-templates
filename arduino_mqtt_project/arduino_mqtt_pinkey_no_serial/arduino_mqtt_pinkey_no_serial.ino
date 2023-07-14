2168198ec423e6b6ce8d0a6525fd8086d9f5a649
antonioc76 <antoniochappell@protonmail.com>
2023-07-14 09:26:08 -0500

    added example virtual parameter that controls feedback on analog input line updates

--- arduino_mqtt_project/arduino_mqtt_pinkey_no_serial/arduino_mqtt_pinkey_no_serial.ino
+++ arduino_mqtt_project/arduino_mqtt_pinkey_no_serial/arduino_mqtt_pinkey_no_serial.ino
@@ -5,7 +5,9 @@
 
 // PubSubClient
 // set your mac address with each term as a base 16 byte
-byte mac[] = {0x02, 0x42, 0x0a, 0x60, 0x15, 0x65};
+// 98:76:B6:11:2D:14
+// byte mac[] = {0x02, 0x42, 0x0a, 0x60, 0x15, 0x65};
+byte mac[] = {0x98, 0x76, 0xB6, 0x11, 0x2D, 0x14};
 
 // set the ip address of the server
 IPAddress server(192, 168, 4, 121);
@@ -14,7 +16,8 @@ PubSubClient client(ethClient);
 
 /**** PIN STRUCTURE ARRAY ****/
 // set number_pins equal to the number of pinouts you want to define
-constexpr uint8_t number_pins = 9;
+constexpr uint8_t number_pins = 14;
+constexpr uint8_t number_Vpins = 1;
 
 // struct for associating pin logical names with pinouts
 struct Pin {
@@ -28,18 +31,26 @@ struct Pin {
 
 // IMPORTANT
 // add your pinout structs to the array in the form {"logical_name", pinout}
+// boolean configuration parameters can be defined here as well, and 1 (true) or 0 (false) can be sent over mqtt to turn them on or off
 // order from high priority in search to low priority
 Pin feather_pins[number_pins] = { 
   {"D5", 5, "digital", 1, 1},
   {"D6", 6, "digital", 1, 1},
   {"D9", 9, "digital", 0, 0},
-  {"A0", A0, "digital", 0, 0},
+  {"D10", 10, "digital", 0, 0},
+  {"D11", 11, "digital", 0, 0},
+  {"D12", 12, "digital", 0, 0},
+  {"D13", 13, "digital", 0, 0},
+  {"A0", A0, "digital", 0, 0}, // ethernet pin by default
   {"A1", A1, "analog", 0, 0},
   {"A2", A2, "analog", 0, 0},
   {"A3", A3, "analog", 0, 0},
   {"A4", A4, "analog", 0, 0},
-  {"A5", A5, "analog", 0, 0},
-  
+  {"A5", A5, "analog", 0, 0}
+};
+
+Pin virtual_configuration_pins[number_Vpins] = {
+  {"analogs_tied_down", 999, "virtual_configuration", 0, 0} // set to 1 if your analogs are tied down, allows feather to publish current values when an analog input line changes
 };
 
 // searches the pinout array for the pin with the name called in the Json packet
@@ -67,15 +78,26 @@ const char* contains_pin(Pin Pins[], JsonDocument* json_doc) {
       return Pins[i].name;
     } 
   }
+  return "virtual pin";
 }
 
 // check if current values in memory for digital pins are the same as on their physical lines
-bool are_current_digital_values_same(Pin Pins[]) {
-  for (uint8_t i=0; i<number_pins; i++) {
-    if (Pins[i].pin_type == "digital") {
-      
-      // at least one of the digital pins values are different
-      if (Pins[i].current_value != digitalRead(Pins[i].pin_number)) {
+bool are_current_digital_values_same(Pin Pins[], Pin Vpin) {
+  if (Vpin.current_value == 0) {
+    for (uint8_t i=0; i<number_pins; i++) {
+      if (Pins[i].pin_type == "digital") {
+        
+        // at least one of the digital pins values are different
+        if (Pins[i].current_value != digitalRead(Pins[i].pin_number)) {
+          return false;
+        }
+      }  
+    }
+    // if analog pins are not tied down this will DDOS your mqtt server
+  } else if (Vpin.current_value != 0) { // if your analogs are tied down, this function will return true when any pin, digital or analog, has a different written value
+    for (uint8_t i=0; i<number_pins; i++) {
+      // at least one of the pins values are different
+      if (Pins[i].current_value != analogRead(Pins[i].pin_number)) {
         return false;
       }
     }  
@@ -135,6 +157,19 @@ void produce_current_msg(Pin Pins[], PubSubClient client, char* topic, unsigned 
   pub_doc.clear();
 }
 
+// sets values of virtual pins
+void set_virtual_pin_values (Pin Vpins[], const char* name, uint8_t value) {
+  for (uint8_t i=0;i<number_Vpins;i++) {
+    if (strncmp(Vpins[i].name, name, 20) == 0) {
+      Serial.println("virtual pin: ");
+      Serial.print(Vpins[i].name);
+      Serial.print(" getting set to ");
+      Serial.println(value);
+      Vpins[i].current_value = value;  
+    }
+  }
+}
+
 /**** INCOMING MQTT PAYLOAD ****/
 // callback on MQTT payload received
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -150,6 +185,13 @@ void callback(char* topic, byte* payload, unsigned int length) {
   doc_pointer = &doc;
   pin = contains_pin(feather_pins, doc_pointer);
   value = doc[pin];
+
+  if (strncmp(pin, "virtual pin", 15) == 0) {
+    pin = contains_pin(virtual_configuration_pins, doc_pointer);
+    value = doc[pin];
+    set_virtual_pin_values(virtual_configuration_pins, pin, value);
+  }
+  
   if (strncmp(get_pin_type(feather_pins, pin), "digital", 10) == 0)  {
     digitalWrite(get_pin_number(feather_pins, pin), value);
   }
@@ -159,7 +201,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
   // publish the current values on the pin lines to the mqtt server
   // set_pin_current_values(feather_pins);
-  if (!are_current_digital_values_same(feather_pins)) {
+  if (!are_current_digital_values_same(feather_pins, virtual_configuration_pins[0])) {
     set_pin_current_values(feather_pins);
     produce_current_msg(feather_pins, client, "pins/on_change/current", 20);
   }
@@ -219,10 +261,11 @@ void loop() {
     reconnect();
   }
   
-  if (!are_current_digital_values_same(feather_pins)) {
+  if (!are_current_digital_values_same(feather_pins, virtual_configuration_pins[0])) {
     Serial.println(F("current values are NOT the same"));
     set_pin_current_values(feather_pins);
     produce_current_msg(feather_pins, client, "pins/on_change/current", 20);
+    produce_current_msg(feather_pins, client, "pins/current", 20);
   }
 
   set_pin_current_values(feather_pins);
